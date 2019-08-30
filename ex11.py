71ef957f78a5968ac437b76afde97dcc9ec5fe05
vagrant <abluepython@e-tailors.com>
2019-08-30 06:09:36 +0000

    second commit

---
+++ ex11.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+print("How old are you?", end=' ')
+age = input()
+
+print("How tall are you?", end=' ')
+height = input()
+
+print("How much do you wight?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
